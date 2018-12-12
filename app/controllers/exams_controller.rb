e4e8011e6ede4c9a2a4abfe753c7669bc8d5ce1c
moridai <your.mori@renoveru.jp>
2018-12-12 13:14:49 +0900

    show one question

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,7 +3,19 @@ class ExamsController < ApplicationController
   end
 
   def question
-    @exams = Exam.all.order("RANDOM()").limit(2)
+    # 実装アイデア
+    # データベースの全データの中からランダムに2レコードを取得する
+    # 取得したレコードを@exams変数に入れる
+    # @exam変数に取得したレコードのうち1番目のレコードを入れる
+    # 1問目は1番目のレコードの問題と選択肢を表示する
+    # 1問目のresult画面でquestionアクションを呼び出す時にどうにかして2番目のレコードを@exam変数に入れる
+    # 2問目を表示する際は2番目のレコードの問題と選択肢を表示する
+    @exams = Exam.all.limit(2)
+    if params[:next_question] then
+      @exam = @exams[1]
+    else
+      @exam = @exams[0]
+    end
   end
 
   def result
