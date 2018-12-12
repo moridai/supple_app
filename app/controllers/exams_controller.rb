39fc73498ab539b83b2b0ae48cf76f9f6a5dd1cd
moridai <your.mori@renoveru.jp>
2018-12-12 14:06:43 +0900

    next question done

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,13 +3,6 @@ class ExamsController < ApplicationController
   end
 
   def question
-    # 実装アイデア
-    # データベースの全データの中からランダムに2レコードを取得する
-    # 取得したレコードを@exams変数に入れる
-    # @exam変数に取得したレコードのうち1番目のレコードを入れる
-    # 1問目は1番目のレコードの問題と選択肢を表示する
-    # 1問目のresult画面でquestionアクションを呼び出す時にどうにかして2番目のレコードを@exam変数に入れる
-    # 2問目を表示する際は2番目のレコードの問題と選択肢を表示する
     @exams = Exam.all.limit(2)
     if params[:next_question] then
       @exam = @exams[1]
@@ -19,7 +12,9 @@ class ExamsController < ApplicationController
   end
 
   def result
-    @answer = Exam.all.first.answer
+    @examid = params[:id]
+    @exam = Exam.find_by(id: @examid)
+    @answer = @exam.answer
     if @answer == params[:select] then
       @msg = '正解'
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'exams#home'
   get 'exams/question'
+  post 'exams/question'
   get 'exams/result'
   post 'exams/result'
 end
